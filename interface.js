5f103df8e9c67f9755a2ade7abd2577ec32cc003
Marcus Rands <mrands@hotmail.co.uk>
2017-11-28 15:32:08 +0000

    All buttons now linked up to functions in interface

--- interface.js
+++ interface.js
@@ -1,9 +1,37 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $('#temperature').text(thermostat._temperature);
+  updateTemperature();
 
   $('#temperature-up').click(function() {
-  thermostat.up();
-  $('#temperature').text(thermostat.getTemperature());
-});
+    thermostat.up();
+    updateTemperature();
+  })
+
+  $('#temperature-down').click(function() {
+    thermostat.down();
+    updateTemperature();
+  })
+
+  $('#temperature-reset').click(function() {
+    thermostat.resetTemperature();
+    updateTemperature();
+  })
+
+  $('#powersaving-on').click(function() {
+    thermostat.turnPowerSavingModeOn();
+    $('#power-saving-status').text('on')
+    updateTemperature();
+  })
+
+  $('#powersaving-off').click(function() {
+    thermostat.turnPowerSavingModeOff();
+    $('#power-saving-status').text('off')
+    updateTemperature();
+  })
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.getTemperature());
+  }
+
+
 });
