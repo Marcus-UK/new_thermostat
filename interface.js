725a6fea5c79d0133319f8e1fc21aaf699d2593e
Marcus Rands <mrands@hotmail.co.uk>
2017-11-28 16:19:17 +0000

    Added css file. Temperature color now updates dynamically based on current temp

--- interface.js
+++ interface.js
@@ -31,6 +31,7 @@ $(document).ready(function() {
 
   function updateTemperature() {
     $('#temperature').text(thermostat.getTemperature());
+    $('#temperature').attr('class', thermostat.energyUsage());
   }
 
 
